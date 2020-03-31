3e6f1be3ad184c134d7d2a06369e3c6c48810e7d
Nadeesha Dilruwan <eclayaz@gmail.com>
2020-03-31 18:52:30 +0530

    two way data binding

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -3,11 +3,32 @@ import SearchBar from "./SearchBar";
 import ProductTable from "./ProductTable";
 
 class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inStock: false,
+      search: ""
+    };
+  }
+
+  handleInStock = () => {
+    this.setState({ inStock: !this.state.inStock });
+  };
+
+  handleSearch = val => {
+    this.setState({ search: val });
+  };
+
   render() {
     return (
       <div>
         <h2>Product List</h2>
-        <SearchBar />
+        <SearchBar
+          inStock={this.state.inStock}
+          search={this.state.search}
+          handleInStock={this.handleInStock}
+          handleSearch={this.handleSearch}
+        />
         <br /> <br />
         <ProductTable productList={this.props.productList} />
       </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,28 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  handleInStockChange = e => {
+    this.props.handleInStock();
+  };
+
+  handleSearchChange = e => {
+    this.props.handleSearch(e.target.value);
+  };
+
   render() {
     return (
       <div>
-        <input type="text" />
+        <input
+          type="text"
+          value={this.props.search}
+          onChange={this.handleSearchChange}
+        />
         <br />
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={this.props.inStock}
+          onChange={this.handleInStockChange}
+        />
         <span>Only show products in stock</span>
       </div>
     );
